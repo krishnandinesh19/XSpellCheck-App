4f2c9f86e9ea9c7583e58de89a7c739314d89673
krishnandinesh19 <dkkrishnan20@gmail.com>
2024-03-16 18:57:23 +0400

    main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import XSpellCheck from "./spellcheck";
+
+function App() {
+  return (
+    <div className="App">
+      <XSpellCheck />
+    </div>
+  );
+}
+
+export default App;
